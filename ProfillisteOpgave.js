a6bee332c7103687d5102331e8076cc3623b08d5
Frank Goldmann <frg@rts.dk>
2017-08-18 12:37:08 +0200

    Create ProfillisteOpgave.js

---
+++ ProfillisteOpgave.js
@@ -0,0 +1,26 @@
+// profilopgaven
+var profilopgave = function (p, id = -1) {
+    var profil = "";
+    if (id === -1) {
+        profil = "alle profiler\n";
+        p.forEach(function (element, index) {
+            //profil += `${element[0]} ${element[1]} ${element[2]}  "\n"`;
+            element.forEach(function (item) {
+                profil += `${item} `;
+            });
+            profil += "\n";
+        });
+    }
+    else {
+        profil = "\nEn profil\n";
+        p[id].forEach(function (element, index) {
+            profil += element + ' ';
+        });
+    }
+    return profil
+}
+
+var personliste = [["Jens", "Hansen", 1970], ["Tina", "Jensen", 1982]];
+
+console.log(profilopgave(personliste));
+console.log(profilopgave(personliste, 1));
